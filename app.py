eb23473485feb13e86062088ee3419c6591e3886
DavidV <davidvargasmendiola@gmail.com>
2024-10-29 18:15:19 -0600

    corrigiendo codigo para heroku

--- app.py
+++ app.py
@@ -2,28 +2,20 @@ from flask import Flask, render_template, request, redirect
 from google.oauth2 import service_account
 import gspread
 from datetime import datetime
-import os
-import json
+import os  # Importa el módulo os para obtener el puerto de Heroku
 
 app = Flask(__name__)
 
-# Leer la variable de entorno para las credenciales
-creds_json = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-creds_info = json.loads(creds_json)
-
-# Crear las credenciales usando la información JSON
-creds = service_account.Credentials.from_service_account_info(
-    creds_info,
+creds = service_account.Credentials.from_service_account_file(
+    'sugerencias-de-canciones-794434182960.json',
     scopes=["https://www.googleapis.com/auth/spreadsheets"]
 )
 
-# Autenticación con Google Sheets
 client = gspread.authorize(creds)
 
-# Abre la hoja de cálculo usando el ID
-spreadsheet_id = "1NrgGLDDv5oZV9qSfRP4LrrVo039zJMPP7ahRf2g7L6k"  # Reemplaza esto con tu ID de hoja de cálculo
+spreadsheet_id = "1NrgGLDDv5oZV9qSfRP4LrrVo039zJMPP7ahRf2g7L6k"
 spreadsheet = client.open_by_key(spreadsheet_id)
-worksheet = spreadsheet.sheet1  # Selecciona la primera hoja
+worksheet = spreadsheet.sheet1
 
 @app.route('/')
 def index():
@@ -33,12 +25,10 @@ def index():
 def enviar():
     cancion = request.form['cancion'].upper()
     artista = request.form['artista'].upper()
-    fecha_sugerencia = datetime.now().strftime('%d-%m-%Y')  # Captura la fecha actual en formato DD-MM-AAAA
-    
-    # Agregar datos a la hoja de cálculo
+    fecha_sugerencia = datetime.now().strftime('%d-%m-%Y')
     worksheet.append_row([cancion, artista, "📩 Enviada", fecha_sugerencia])
-
-    return redirect('/')  # Redirige de vuelta al formulario
+    return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Obtiene el puerto de Heroku o usa 5000 localmente
+    app.run(debug=True, host='0.0.0.0', port=port)
